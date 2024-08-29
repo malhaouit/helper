7d322733afe7a012a65195f321222b260b5f9290
malhaouit <abdelmounaim.abdel@gmail.com>
2024-08-29 15:27:34 -0700

    App.jsx

--- App.jsx
+++ App.jsx
@@ -1,49 +1,26 @@
+// App.jsx
+
 import React, { useState } from 'react';
 import StartGame from './components/StartGame';
 import GameBoard from './components/GameBoard';
-import GameOver from './components/GameOver';
 
 const App = () => {
   const [gameId, setGameId] = useState(null);
-  const [player1Id, setPlayer1Id] = useState(null);
-  const [player2Id, setPlayer2Id] = useState(null);
-  const [gameOver, setGameOver] = useState(false);
-  const [winner, setWinner] = useState(null);
+  const [player1, setPlayer1] = useState(null);
+  const [player2, setPlayer2] = useState(null);
 
-  const handleStartGame = (gameData) => {
-    setGameId(gameData.id);
-    setPlayer1Id(gameData.first_player_id);
-    setPlayer2Id(gameData.second_player_id);
-  };
-
-  const handleGameOver = (winner) => {
-    setGameOver(true);
-    setWinner(winner);
-  };
-
-  const handleRestart = () => {
-    setGameOver(false);
-    setWinner(null);
-    setGameId(null);
-    setPlayer1Id(null);
-    setPlayer2Id(null);
+  const handleStartGame = (newGameId, player1, player2) => {
+    setGameId(newGameId);
+    setPlayer1(player1);
+    setPlayer2(player2);
   };
 
   return (
-    <div className="app">
-      {!gameId && !gameOver && (
-        <StartGame onStartGame={handleStartGame} />
-      )}
-      {gameId && !gameOver && (
-        <GameBoard
-          gameId={gameId}
-          player1Id={player1Id}
-          player2Id={player2Id}
-          onGameOver={handleGameOver}
-        />
-      )}
-      {gameOver && (
-        <GameOver winner={winner} onRestart={handleRestart} />
+    <div>
+      {gameId ? (
+        <GameBoard gameId={gameId} player1={player1} player2={player2} />
+      ) : (
+        <StartGame onStart={handleStartGame} />
       )}
     </div>
   );
