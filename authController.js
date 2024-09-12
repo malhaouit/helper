8cbae7397479435739e169c1a177f012086d1918
malhaouit <abdelmounaim.abdel@gmail.com>
2024-09-12 23:21:23 +0100

    authController.js

--- authController.js
+++ authController.js
@@ -1,13 +1,17 @@
-const bcrypt = require('bcryptjs');
+const crypto = require('crypto');
+const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');  // Use the Mongoose model
 const emailService = require('../utils/email');
+const { OAuth2Client } = require('google-auth-library');
 const dotenv = require('dotenv');
 dotenv.config();
+const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
+
 
 // Register a new user
 exports.register = async (req, res) => {
-    const { name, email, password } = req.body;
+    const { name, email, password, confirmPassword } = req.body;
 
     try {
         console.log('Starting registration process');
@@ -19,67 +23,78 @@ exports.register = async (req, res) => {
             return res.status(400).json({ msg: 'User already exists' });
         }
 
+	// Check if password and confirmPassword match
+	if (password !== confirmPassword) {
+	    return res.status(400).json({ msg: 'Passwords do not match' });
+	}
         // Hash the password before saving
         const hashedPassword = await bcrypt.hash(password, 10);
+
+	// Generate email confirmation token
+        const confirmationToken = crypto.randomBytes(20).toString('hex');
 
         // Create a new user instance
         user = new User({
             name,
             email,
-            password: hashedPassword
+            password: hashedPassword,
+	    confirmationToken
         });
 
         // Save the new user in the database
         await user.save();
-        console.log('User inserted into database');
 
-        // Send registration email
-        await emailService.sendRegistrationEmail(email, name);
-        console.log('Registration email sent');
-
-        // Create a JWT payload and sign it
-        const payload = {
-            user: {
-                id: user._id,
-                email: user.email,
-                name: user.name
-            }
-        };
-
-        jwt.sign(
-            payload,
-            process.env.JWT_SECRET,
-            { expiresIn: '1h' },
-            (err, token) => {
-                if (err) {
-                    console.error('JWT signing error:', err);
-                    throw err;
-                }
-                console.log('JWT generated');
-                res.json({ token });
-            }
-        );
+	// Send confirmation email
+        const confirmLink = `${process.env.FRONTEND_URL}/confirm/${confirmationToken}`;
+	await emailService.sendConfirmationEmail(email, confirmLink);
     } catch (err) {
         console.error('Register error:', err.message);
         res.status(500).send('Server error');
     }
 };
 
+exports.confirmEmail = async (req, res) => {
+    const { token } = req.params;
+
+    try {
+       const user = await User.findOne({ confirmationToken: token });
+       if (!user) {
+           return res.status(400).json({ msg: 'Invalid or expired confirmation token' });
+       }
+
+       user.isEmailConfirmed = true;
+       user.confirmationToken = undefined;
+       await user.save();
+
+       return res.status(200).json({ msg: 'Email confirmed successfully' });
+    } catch (err) {
+       console.error('Confirm email error:', err.message);
+       return res.status(500).send('Server error');
+    }
+};
+    
 // Login a user
 exports.login = async (req, res) => {
     const { email, password } = req.body;
+    console.log(`Attempting login for email: ${email}`); // Log the input email
 
     try {
         // Find the user using Mongoose
-        const user = await User.findOne({ email });
+        const user = await User.findOne({ email: email.toLowerCase() });
         if (!user) {
-            return res.status(400).json({ msg: 'Invalid credentials' });
+	    console.log('User not found');
+            return res.status(400).json({ msg: 'Invalid email' });
         }
+
+        if (!user.isEmailConfirmed) {
+            return res.status(400).json({ msg: 'Please confirm your email before logging in' });
+	}
 
         // Compare the provided password with the hashed password in the database
         const isMatch = await bcrypt.compare(password, user.password);
+	console.log('Password match:', isMatch); // Log if password comparison succeeded
         if (!isMatch) {
-            return res.status(400).json({ msg: 'Invalid credentials' });
+            return res.status(400).json({ msg: 'Invalid password' });
         }
 
         // If password matches, generate a JWT token
@@ -97,13 +112,69 @@ exports.login = async (req, res) => {
             { expiresIn: '1h' },
             (err, token) => {
                 if (err) throw err;
-                res.json({ token }); // Send token in response
-            }
+                res.json({
+                    token,
+                    user: {
+                      id: user._id,
+                      email: user.email,
+                      name: user.name,
+                    },
+                });             }
         );
     } catch (err) {
         console.error('Error in login controller:', err.message);
         res.status(500).send('Server error in login controller');
     }
+};
+
+exports.googleLogin = async (req, res) => {
+  const { idToken } = req.body;
+
+  try {
+    // Verify the token received from Google
+    const ticket = await client.verifyIdToken({
+      idToken,
+      audience: process.env.GOOGLE_CLIENT_ID,
+    });
+    const payload = ticket.getPayload();
+
+    // Find or create the user
+    let user = await User.findOne({ email: payload.email });
+    if (!user) {
+      user = new User({
+        googleId: payload.sub,
+        name: payload.name,
+        email: payload.email,
+        isEmailConfirmed: true, // Automatically confirm email for Google logins
+      });
+      await user.save();
+    }
+
+    // Generate JWT token for the user
+    const token = jwt.sign(
+      {
+        user: {
+          id: user._id,
+          email: user.email,
+          name: user.name,
+        },
+      },
+      process.env.JWT_SECRET,
+      { expiresIn: '1h' }
+    );
+
+    res.status(200).json({
+        token,
+        user: {
+          id: user._id,
+          email: user.email,
+          name: user.name,
+        },
+      });
+  } catch (error) {
+    console.error('Google login error:', error.message);
+    res.status(500).send('Google login error');
+  }
 };
 
 // Send password reset email
@@ -125,7 +196,7 @@ exports.resetPassword = async (req, res) => {
         );
 
         // Create the reset link and send email
-        const resetLink = `${process.env.BASE_URL}/reset-password/${resetToken}`;
+	const resetLink = `http://localhost:5173/reset-password/${resetToken}`;
         await emailService.sendPasswordResetEmail(email, resetLink);
 
         res.json({ msg: 'Password reset email sent' });
