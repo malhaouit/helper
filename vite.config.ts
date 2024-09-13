a27dc7e6d1c9170d60be6b4bb34f6de15f791aa9
malaouit <abdelmounaim.abdel@gmail.com>
2024-09-13 08:00:51 -0700

    hi

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',  // Base set for root deployment
+  base: '/',
   build: {
-    outDir: 'dist',  // Ensure Vite outputs build files to 'dist'
+    outDir: 'dist',
   },
 });
