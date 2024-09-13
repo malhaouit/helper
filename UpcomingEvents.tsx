e0e10510c4229cade754c0f0f94d59825b1b7d7d
malaouit <abdelmounaim.abdel@gmail.com>
2024-09-13 06:48:32 -0700

    hi

--- UpcomingEvents.tsx
+++ UpcomingEvents.tsx
@@ -1,94 +1,94 @@
-import './UpcomingEvents.css'
+import './UpcomingEvents.css';
 
-import cyber_security from '../../assets/cyber_security.png'
-import Artificial_intelligence from '../../assets/Artificial_intelligence.jpeg'
-import Data_science from '../../assets/Data_science.jpg'
-import Python_course from '../../assets/Python_course.jpg'
-import { MdDateRange } from "react-icons/md";
-import { TbClockHour10 } from "react-icons/tb";
+import cyber_security from '../../assets/cyber_security.png';
+import Artificial_intelligence from '../../assets/Artificial_intelligence.jpeg';
+import Data_science from '../../assets/Data_science.jpg';
+import Python_course from '../../assets/Python_course.jpg';
+import { MdDateRange } from 'react-icons/md';
+import { TbClockHour10 } from 'react-icons/tb';
+
+// Define the Event type
+type Event = {
+  id: string;
+  title: string;
+  category: string;
+  date: string;
+  time: string;
+  image: string;
+};
+
+const upcomingEvents: Event[] = [
+  {
+    id: '1',
+    title: 'Artificial Intelligence course',
+    category: 'Education',
+    date: '15 Sept 2024',
+    time: '9:00',
+    image: Artificial_intelligence,
+  },
+  {
+    id: '2',
+    title: 'Cyber Security course',
+    category: 'Education',
+    date: '15 Sept 2024',
+    time: '9:00',
+    image: cyber_security,
+  },
+  {
+    id: '3',
+    title: 'Data Science course',
+    category: 'Education',
+    date: '15 Sept 2024',
+    time: '9:00',
+    image: Data_science,
+  },
+  {
+    id: '4',
+    title: 'Python Course',
+    category: 'Education',
+    date: '15 Sept 2024',
+    time: '9:00',
+    image: Python_course,
+  },
+];
 
 const UpcomingEvents = () => {
   return (
-    <div className='section-wrapper'>
-       <div className='section-header'>
-        <h4> Upcoming Events</h4>
-       </div>
-       <div className='UpcomingEvents-items'>
-        {/* Artificial Intelligence Event */}
-        <div className='UpcomingEvents-item'>
-            <div className='card-wrapper'>
-                <img className='UpcomingEvents-item-image' src={Artificial_intelligence}/>
-                <div className='UpcomingEvents-item-content'>
-                    <h4 className='UpcomingEvents-item-title'>
-                        Artificial Intelligence course <br />
-                        <span> Education</span>
-                    </h4>
-                    <ul>
-                        <li><MdDateRange /> <span> 15 Sept 2024 </span></li>
-                        <li><TbClockHour10 /><span> 9:00</span></li>
-                    </ul>
-                </div>
+    <div className="section-wrapper">
+      <div className="section-header">
+        <h4>Upcoming Events</h4>
+      </div>
+      <div className="UpcomingEvents-items">
+        {upcomingEvents.map((event) => (
+          <div className="UpcomingEvents-item" key={event.id}>
+            <div className="card-wrapper">
+              <img className="UpcomingEvents-item-image" src={event.image} alt={event.title} />
+              <div className="UpcomingEvents-item-content">
+                <h4 className="UpcomingEvents-item-title">
+                  {event.title} <br />
+                  <span>{event.category}</span>
+                </h4>
+                <ul>
+                  <li>
+                    <MdDateRange /> <span>{event.date}</span>
+                  </li>
+                  <li>
+                    <TbClockHour10 />
+                    <span>{event.time}</span>
+                  </li>
+                </ul>
+              </div>
             </div>
-        </div>
+          </div>
+        ))}
+      </div>
 
-        {/* Cyber Security Event */}
-        <div className='UpcomingEvents-item'>
-            <div className='card-wrapper'>
-                <img className='UpcomingEvents-item-image' src={cyber_security}/>
-                <div className='UpcomingEvents-item-content'>
-                    <h4 className='UpcomingEvents-item-title'>
-                        Cyber Security course <br />
-                        <span> Education</span>
-                    </h4>
-                    <ul>
-                        <li><MdDateRange /> <span> 15 Sept 2024 </span></li>
-                        <li><TbClockHour10 /><span> 9:00</span></li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-
-        {/* Data Science Event */}
-        <div className='UpcomingEvents-item'>
-            <div className='card-wrapper'>
-                <img className='UpcomingEvents-item-image' src={Data_science}/>
-                <div className='UpcomingEvents-item-content'>
-                    <h4 className='UpcomingEvents-item-title'>
-                        Data Science course<br />
-                        <span> Education</span>
-                    </h4>
-                    <ul>
-                        <li><MdDateRange /> <span> 15 Sept 2024 </span></li>
-                        <li><TbClockHour10 /><span> 9:00</span></li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-
-        {/* Python Course Event */}
-        <div className='UpcomingEvents-item'>
-            <div className='card-wrapper'>
-                <img className='UpcomingEvents-item-image' src={Python_course}/>
-                <div className='UpcomingEvents-item-content'>
-                    <h4 className='UpcomingEvents-item-title'>
-                        Python Course<br />
-                        <span> Education</span>
-                    </h4>
-                    <ul>
-                        <li><MdDateRange /> <span> 15 Sept 2024 </span></li>
-                        <li><TbClockHour10 /><span> 9:00</span></li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-       </div>
-
-       {/* Button Container */}
-       <div className='UpcomingEvents-button-container'>
-         <button className='more-events-button'>More Events</button>
-       </div>
+      {/* Button Container */}
+      <div className="UpcomingEvents-button-container">
+        <button className="more-events-button">More Events</button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default UpcomingEvents;
